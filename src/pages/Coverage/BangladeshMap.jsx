c4879a61db9903dcc66e1f348ad5e92eeb516264
smbmunna <smbmunna@gmail.com>
2025-11-07 21:03:14 +0600

    Search functionality added to the Coverage and BangladeshMap component

--- src/pages/Coverage/BangladeshMap.jsx
+++ src/pages/Coverage/BangladeshMap.jsx
@@ -1,12 +1,29 @@
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 
-const position = [23.8103, 90.4125]; 
+const position = [23.8103, 90.4125];
 
-const BangladeshMap = ({ serviceCenters }) => {
+const BangladeshMap = ({ serviceCenters, selectedDistrict }) => {
 
-    //console.log([serviceCenters[0].latitude, serviceCenters[0].latitude]);
+    //Helper component to control pan
+    const MapController= ({selectedDistrict})=>{
+        const map= useMap(); 
+        useEffect(()=>{
+            if(selectedDistrict){
+                map.flyTo(
+                    [selectedDistrict.latitude, selectedDistrict.longitude],
+                    10, //zoom level
+                    {
+                        duration: 2
+                    }
+                )
+            }
+        },[selectedDistrict, map])
+
+        return null; 
+    }
 
     return (
         <MapContainer center={position} zoom={10} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}
@@ -15,12 +32,15 @@ const BangladeshMap = ({ serviceCenters }) => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <MapController selectedDistrict={selectedDistrict}/>
             {
-                serviceCenters.map((center, idx)=>(<Marker key={idx} position={[center.latitude, center.longitude]}>
-                <Popup>
-                    A pretty CSS3 popup. <br />
-                </Popup>
-            </Marker>))
+                serviceCenters.map((center, idx) => (<Marker key={idx} position={[center.latitude, center.longitude]}>
+                    <Popup>
+                        <strong>{center.district}</strong>
+                        <br/>
+                        {center.name}
+                    </Popup>
+                </Marker>))
             }
         </MapContainer>
     )
--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -30,9 +30,9 @@ const Coverage = () => {
         }
     }
 
-    return <div className="mx-8">
-        <h1 className="text-left  text-[#03373D] text-2xl font-semibold">We are available in 64 districts. </h1>
-        <div className="flex my-16 gap-2">
+    return <div className="mx-8 mb-16">
+        <h1 className="my-4 text-[#03373D] text-2xl font-semibold">We are available in 64 districts. </h1>
+        <div className="flex my-4 gap-2 justify-center">
             <input
                 className="border rounded-xl p-2 border-gray-400"
                 type="text"
@@ -47,7 +47,7 @@ const Coverage = () => {
                 onClick={handleSearch}
             >Search</button>
         </div>
-        <h1 className="text-left  text-[#03373D] text-lg font-semibold my-8">We deliver almost all over Bangladesh</h1>
+        <h1 className="text-[#03373D] text-lg font-semibold my-8">We deliver almost all over Bangladesh</h1>
         <BangladeshMap
             serviceCenters={serviceCenters}
             selectedDistrict= {selectedDistrict}
