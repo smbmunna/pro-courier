6ab6ee4dd9774eb53d5dd0300637cd799bba9e79
smbmunna <smbmunna@gmail.com>
2025-11-10 01:17:55 +0600

    Sender and receiver columns added in the react hook form

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -3,8 +3,8 @@ import { useForm } from "react-hook-form";
 
 const SendParcel = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const formSubmit = () => {
-        console.log('form submitted');
+    const formSubmit = (data) => {
+        console.log(data);
     }
     return (
         <div className="mb-8">
@@ -13,31 +13,202 @@ const SendParcel = () => {
             <h2 className="m-4 text-left text-[#03373D] text-xl font-semibold">Enter your parcel details: </h2>
 
             {/* form section */}
-            <form onSubmit={handleSubmit(formSubmit)}>
+            <form onSubmit={handleSubmit(formSubmit)} className="mx-8">
                 <div className="flex ml-4 mb-8">
                     <input
                         type='radio'
                         value='document'
                         className="mr-2"
+                        {...register('parcelType', { required: "Please select a parcel type!" })}
                     />
                     <span className="mr-8">Document</span>
 
                     <input type="radio"
                         value='non-document'
                         className="mx-2"
+                        {...register('parcelType', { required: 'Please select a parcel type!' })}
                     />
                     <span>Non-Document</span>
+                    <br />
+                    <div>
+                        {errors.parcelType && (
+                            <p className="text-red-500 text-sm text-left ml-4">{errors.parcelType.message}</p>
+                        )}
+                    </div>
                 </div>
+
                 <div className="grid grid-cols-1 justify-items-start ml-4 md:grid-cols-2 gap-8">
                     <div className="w-full">
                         {/* parcel Name */}
                         <label className="block text-left my-2">Parcel Name</label>
-                        <input className="border border-gray-300 rounded-md p-2 w-full" type='text' placeholder="Parcel Name" />
+                        <input
+                            className="border border-gray-300 rounded-md p-2 w-full"
+                            type='text'
+                            placeholder="Parcel Name"
+                            {...register('parcelName', { required: 'Parcel name is required!' })}
+                        />
+                        {errors.parcelName && (
+                            <p className="text-red-500 text-sm mt-2">{errors.parcelName.message}</p>
+                        )}
                     </div>
                     <div className="w-full">
                         {/* parcel Weight */}
                         <label className="block text-left my-2">Parcel Weight (KG)</label>
-                        <input className="border border-gray-300 rounded-md p-2 w-full" type='text' placeholder="Parcel Weight" />
+                        <input
+                            className="border border-gray-300 rounded-md p-2 w-full"
+                            type='text'
+                            placeholder="Parcel Weight"
+                            {...register('parcelWeight', { required: 'Parcel weight is required!' })}
+                        />
+                        {errors.parcelWeight && (
+                            <p className="text-red-500 text-sm mt-2">{errors.parcelWeight.message}</p>
+                        )}
+                    </div>
+                </div>
+                {/* Horizontal Divider */}
+                <hr className="border-gray-400 my-6" />
+                <div className="grid grid cols-1 md:grid-cols-2 gap-8">
+                    {/* Sender details */}
+                    <div className="w-full">
+                        <h1 className="text-left text-[#03373D] text-lg font-semibold mb-2">Sender Details</h1>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                            <div >
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Sender Name</label>
+                                <input
+                                    {...register('senderName', { required: 'Sender name is required!' })}
+                                    className="border border-gray-300 rounded-md p-2 w-full"
+                                    type="text"
+                                    placeholder="Sender Name" />
+                                {errors.senderName && (
+                                    <p className="text-red-500 text-sm mt-2">{errors.senderName.message}</p>
+                                )}
+                            </div>
+                            <div>
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Sender Pickup Warehouse</label>
+                                <select
+                                    {...register('senderWarehouse')}
+                                    className="border border-gray-300 rounded-md p-2 w-full">
+                                    <option>Select Warehouse</option>
+                                    <option>Warehouse 1</option>
+                                    <option>Warehouse 2</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                            <div >
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Address</label>
+                                <input
+                                    {...register('senderAddress', { required: 'Sender Address required!' })}
+                                    className="border border-gray-300 rounded-md p-2 w-full"
+                                    type="text"
+                                    placeholder="Address" />
+                                {errors.senderAddress && (
+                                    <p className="text-red-500 text-sm mt-2">{errors.senderAddress.message}</p>
+
+                                )}
+                            </div>
+                            <div >
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Sender Contact No.</label>
+                                <input
+                                    {...register('senderContact', { required: 'Sender contact no. needed!' })}
+                                    className="border border-gray-300 rounded-md p-2 w-full"
+                                    type="text"
+                                    placeholder="Sender Contact No. " />
+                                {errors.senderContact && (
+                                    <p className="text-red-500 text-sm mt-2">{errors.senderContact.message}</p>
+                                )}
+                            </div>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-left text-sm font-medium text-gray-700 mb-2">Sender Region</label>
+                            <select
+                                {...register('senderRegion')}
+                                className="border border-gray-300 rounded-md p-2 w-full">
+                                <option>Select Your Region</option>
+                                <option>Region 1</option>
+                                <option>Region 2</option>
+                            </select>
+                        </div>
+                        <div className="mb-4" >
+                            <label className="block text-left text-sm font-medium text-gray-700 mb-2">Pickup Instruction</label>
+                            <textarea
+                                {...register('senderInstruction')}
+                                className="border border-gray-300 rounded-md p-2 w-full"
+                                type="textarea"
+                                placeholder="Pickup Instruction" />
+                        </div>
+                    </div>
+                    {/* Receiver details */}
+                    <div className="w-full">
+                        <h1 className="text-left text-[#03373D] text-lg font-semibold mb-2">Receiver Details</h1>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                            <div >
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Receiver Name</label>
+                                <input
+                                    {...register('rcvName', { required: 'Receiver name required!' })}
+                                    className="border border-gray-300 rounded-md p-2 w-full"
+                                    type="text"
+                                    placeholder="Receiver Name" />
+                                {errors.rcvName && (
+                                    <p className="text-red-500 text-sm mt-2">{errors.rcvName.message}</p>
+
+                                )}
+                            </div>
+                            <div>
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Receiver Delivery Warehouse</label>
+                                <select
+                                    {...register('rcvWarehouse')}
+                                    className="border border-gray-300 rounded-md p-2 w-full">
+                                    <option>Select Warehouse</option>
+                                    <option>Warehouse 1</option>
+                                    <option>Warehouse 2</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                            <div >
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Receiver Address</label>
+                                <input
+                                    {...register('rcvAddress', { required: 'Receiver address required!' })}
+                                    className="border border-gray-300 rounded-md p-2 w-full"
+                                    type="text"
+                                    placeholder="Address" />
+                                {errors.rcvAddress && (
+                                    <p className="text-red-500 text-sm mt-2">{errors.rcvName.message}</p>
+
+                                )}
+                            </div>
+                            <div >
+                                <label className="block text-left text-sm font-medium text-gray-700 mb-2">Receiver Contact No.</label>
+                                <input
+                                    {...register('rcvContact', { required: 'Receiver contact needed!' })}
+                                    className="border border-gray-300 rounded-md p-2 w-full"
+                                    type="text"
+                                    placeholder="Receiver Contact No. " />
+                                {errors.rcvContact && (
+                                    <p className="text-red-500 text-sm mt-2">{errors.rcvContact.message}</p>
+
+                                )}
+                            </div>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-left text-sm font-medium text-gray-700 mb-2">Receiver Region</label>
+                            <select
+                                {...register('rcvRegion')}
+                                className="border border-gray-300 rounded-md p-2 w-full">
+                                <option>Select Your Region</option>
+                                <option>Region 1</option>
+                                <option>Region 2</option>
+                            </select>
+                        </div>
+                        <div className="mb-4" >
+                            <label className="block text-left text-sm font-medium text-gray-700 mb-2">Delivery Instruction</label>
+                            <textarea
+                                {...register('delInstruction')}
+                                className="border border-gray-300 rounded-md p-2 w-full"
+                                type="textarea"
+                                placeholder="Delivery Instruction" />
+                        </div>
                     </div>
                 </div>
                 <input className="btn mt-8" type='submit' />
